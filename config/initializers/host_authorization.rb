14147467e415fd1966da01acf71a231aac613fba
darius <darius.roberts@gmail.com>
2025-02-27 11:35:10 -0800

    update with potential domain names (browser security requires explicit)

--- config/initializers/host_authorization.rb
+++ config/initializers/host_authorization.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
     config.hosts << "gft100-ccb363de7805.herokuapp.com"
     config.hosts << "waybacktogovt-b8d5c93a578c.herokuapp.com"
+    config.hosts << "waybacktogovt.com"
+    config.hosts << "waybacktogovt.org"
+    config.hosts << "peopledontforget.org"
   end
