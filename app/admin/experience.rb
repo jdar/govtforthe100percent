fb4e01644a61bed800a43537a6c2229f993f7b5c
darius <darius.roberts@gmail.com>
2025-03-12 00:46:29 -0700

    add title to experiences, CONT'D

--- app/admin/experience.rb
+++ app/admin/experience.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Experience do
   permit_params :name, :street, :city, :state, :accessible, :changing_table, :unisex, :directions,
-                :comment, :latitude, :longitude, :country, :edit_id, :approved
+                :comment, :latitude, :longitude, :country, :edit_id, :approved,
+                :zip_code, :federal_agency, :agency_website, :experience, :title, :experience_details, :open_to_contact
 end
--- db/migrate/20250311220202_add_title_to_experiences.rb
+++ db/migrate/20250311220202_add_title_to_experiences.rb
@@ -1,7 +1,7 @@
 class AddTitleToExperiences < ActiveRecord::Migration[7.1]
   def change
     change_table :experiences do |t|
-      t.string  :title, limit: 200,  null: false
+      t.string  :title, limit: 200, null: false, default: "default title"
     end
   end
 end
